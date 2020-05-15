0e4d29a68df1e1571136ff90ce2c7a659a7a3dc9
kerryokimon <kerryokimon@gmail.com>
2020-05-15 03:59:29 +0000

    finished 1e.

--- main.cpp
+++ main.cpp
@@ -114,155 +114,249 @@ struct CarWash //                                   1) define an empty struct fo
 };
 
 
-/*
-1)
-Skateboard
-5 properties:
-    1) Grip tape color
-    2) Hardness of Wheels
-    3) Brand of Trucks
-    4) Age of Bearings
-    5) Type of Wood
-3 things it can do:
-    1) roll
-    2) slide
-    3) flip
- */
 
-/*
-2) Car
-5 properties:	
-    1) Brand of Radio	    
-    2) Number of Tires	    
-    3) Size of Engine	    
-    4) Brand of Rims	    
-    5) Count of Seats	    
-3 things it can do:	
-    1) Drive	    
-    2) Park	   
-    3) Stop
-
- */
-
-/*
-3)
-Computer
-5 properties:
-    1) Feel of keys
-    2) Resolution of Monitor
-    3) Kinds of indictor lights
-    4) Number of buttons
-    5) Speed of Processor
-3 things it can do:
-    1) do taxes
-    2) play music
-    3) display numbers
- */
-
-/*
-4)
-Midi Controller
-5 properties:
-    1) Number of knobs
-    2) Kinds of buttons
-    3) Range of faders
-    4) Size of display
-    5) Types of pads
-3 things it can do:
-    1) control synth
-    2) adjust volume
-    3) change bank
- */
-
-/*
-Thing 5) Filter
-    1) Filter Type
-    2) Range of Frequency
-    3) Amount of Resonance
-    4) Amount of modulation
-    5) Amount of Cutoff
-3 things it can do:
-    1) make a sound darker
-    2) make the sound sharper
-    3) make the sound brighter
-
- */
-
-/*
-Thing 6) Oscillator
-    1) Amount of Frequency Modulation
-    2) Type of Waveform
-    3) Amount of course pitch change
-    4) Sync
-    5) Amount of Volume
-
-3 things it can do:
-    1) Modulate another Oscillator
-    2) Generates a signal
-    3) Converts DC to AC
-
- */
-
-/*
-Thing 7) LFO
-    1) Type of waveform
-    2) Rate of LFO
-    3) Phase start position
-    4) Sync with DAW Tempo
-    5) Polarity
-
-3 things it can do:
-    1) Modulate Cutoff
-    2) Change Pitch
-    3) Adjust Frequency
-
- */
-
-/*
-Thing 8) Display
-    1) Resolution
-    2) Pixel Size
-    3) Width
-    4) Color
-    5) Display Technology
-
-3 things it can do:
-    1) Show settings
-    2) Consume electric
-    3) Glow
-
- */
-
-/*
-Thing 9) FX Section
-    1) Amount of Reverb
-    2) Amount of distortion
-    3) Amount of Wet Signal (dry/wet)
-    4) Delay type
-    5) Reverb type
-
-3 things it can do:
-    1) Make the sound echo
-    2) Reverberate the sound
-    3) Saturate the sound
-
- */
-
-/*
-Thing 10) Synthesizer
-5 properties:
-    1) Filter
-    2) LFO
-    3) Oscillator
-    4) Display
-    5) FX Section
-3 things it can do:
-    1) Oscillate
-    2) Play sounds
-    3) Arpegiate
+//1)
+struct Skateboard
+{//5 properties:
+    //1) Grip tape color
+    std::string gripTapeColor = "Green";
+    //2) Hardness of Wheels
+    int wheelsHardness = 99;
+    //3) Brand of Trucks
+    std::string truckBrand = "Independent";
+    //4) Age of Bearings
+    int bearingAgeYears = 2;
+    //5) Type of Wood
+    std::string woodType = "oak";
+//3 things it can do:
+    //1) roll
+    void rollDownStreet(int speed());
+    //2) slide
+    void railSlide(bool frontSide = true);
+    //3) flip
+    void kickFlip(int degree = 360);
+};
 
 
-*/
+//2) 
+struct Car
+{
+//5 properties:	
+    //1) Brand of Radio	    
+    std::string radioBrand = "Sony";
+    //2) Number of Tires	    
+    int numTires = 4;
+    //3) Size of Engine	    
+    int engineSize = 4600;
+    //4) Brand of Rims	    
+    std::string rimBrand = "MSR";
+    //5) Count of Seats
+    int seatCount = 2;	    
+//3 things it can do:	
+    //1) Drive	
+    void drive(int speed());    
+    //2) Park
+    void park(int parkingStallNumber());	   
+    //3) Stop
+    void stop(int stopDistance());
+};
+ 
+
+
+//3)
+struct Computer
+{
+//5 properties:
+    //1) Feel of keys
+    int keyFeel = 2;
+    //2) Resolution of Monitor
+    int monitorResolution = 1080;
+    //3) Kinds of indictor lights
+    std::string indicatorLightKind = "LED";
+    //4) Number of buttons
+    int buttonCount = 3;
+    //5) Speed of Processor
+    float processorSpeed = 4.0f;
+//3 things it can do:
+    //1) do taxes
+    void runQuickbooks(bool isQbInstalled = true);
+    //2) play music
+    void playMusic(int howLoud = 11); //play it loud!
+    //3) display numbers
+    void displayNumbers(int numberCount());
+};
+
+
+//4)
+struct MidiController
+{
+//5 properties:
+    //1) Number of knobs
+    int knobCount = 16;
+    //2) Kinds of buttons
+    char buttonStyle = 'l';
+    //3) Range of faders
+    int faderRange = 10;
+    //4) Size of display
+    float displaySizeInches = 7.2f;
+    //5) Types of pads
+    std::string padType = "lit";
+
+//3 things it can do:
+    //1) control synth
+    void controlSynth(int midiMessage());
+    //2) adjust volume
+    void adjustVolume(int midiMessage = 7);
+    //3) change bank
+    void changeBank();
+};
+
+
+//Thing 5) 
+struct Filter
+{
+    //1) Filter Type
+    std::string filterType = "LP";
+    //2) Range of Frequency
+    float frequencyRange = 20000.0F;
+    //3) Amount of Resonance
+    int rezAmount = 1; //1-100
+    //4) Amount of modulation
+    int modAmount = 1; //1-100
+    //5) Frequency of Cutoff
+    float cutOffFrequency = 20.0f;
+//3 things it can do:
+    //1) make a sound darker
+    void soundTone(float cutoffFrequency());
+    //2) make the sound sharper
+    void addResonance(int rezAmount());
+    //3) make the sound brighter
+    void cutLowFreq(float cutOffFrequency = 1000);
+};
+
+
+//Thing 6) 
+struct Oscillator
+{
+    //1) Amount of Frequency Modulation
+    int freqMod = 1; //1-10
+    //2) Type of Waveform
+    int waveType = 1; //1=sine 2=saw 3=square
+    //3) Amount of course pitch change
+    int maxPitchChange = 24; //semitones
+    //4) Sync
+    bool syncOn = false;
+    //5) Amount of Volume
+    int maxVolume = 1; //1-11
+//3 things it can do:
+    
+    //1) Modulate another Oscillator
+    bool osc2Mod = true;
+    //2) Generates a signal
+    void signalGen(int frequency());
+    //3) Converts DC to AC
+    bool voltageConversion = true;
+};
+ 
+
+
+//Thing 7) 
+struct LFO
+{
+    //1) Type of waveform
+    int waveType = 1; //1=sine 2=saw 3=square
+    //2) Rate of LFO
+    float LfoRate = 20.0f;
+    //3) Phase start position
+    int phaseStartPos = 1;  //1-100
+    //4) Sync with DAW Tempo
+    bool syncOn = false;
+    //5) Polarity
+    bool bipolarOn = true;
+
+//3 things it can do:
+    //1) Modulate Cutoff
+    void modulateCutoff(float lfoRate(), bool syncOn = false);
+    //2) Change Pitch
+    void changePitch(int pitchChangeAmount());
+    //3) Adjust Frequency
+    void adjustFrequency(int freqHz());
+};
+
+
+//Thing 8) 
+struct Display
+{
+    //1) Resolution
+    int screenRes = 1080;
+    //2) Pixel Size
+    int pixelSize = 2;
+    //3) Width
+    float displayWidth = 7.2f;
+    //4) Color
+    char displayColor = 'b'; //b = blue r = red
+    //5) Display Technology
+    std::string displayTechnology = "LED";
+//3 things it can do:
+    //1) Show settings
+    void displaySettings(char displayColor = 'r');
+    //2) Consume electric
+    void energyConsumption();
+    //3) Glow
+    void displayBritenessDim (bool powerSaver = true, int lumens = 100);
+};
+ 
+
+
+//Thing 9) 
+struct FXSection
+{
+    //1) Amount of Reverb
+    int reverbAmount = 50;
+    //2) Amount of distortion
+    int distAmount = 1;
+    //3) Amount of Wet Signal (dry/wet)
+    int wetSignal = 50;
+    //4) Delay type
+    int delayType = 1; //1 = pingpong 2 = tape
+    //5) Reverb type
+    int reverbType = 1; //1 = plate 2 = spring
+
+//3 things it can do:
+    //1) Make the sound echo
+    bool delayActive = false;
+    //2) Reverberate the sound
+    void reverbOutput(int reverbtype(), bool reverbOn = true);
+    //3) Saturate the sound
+    void saturationActive(int distAmount(), bool wetSignal());
+};
+ 
+
+
+//Thing 10) 
+struct Synthesizer
+{
+//5 properties:
+    //1) Filter
+    Filter filterA;
+    //2) LFO
+    LFO lfo1;
+    //3) Oscillator
+    Oscillator osc1;
+    //4) Display
+    Display display;
+    //5) FX Section
+    FXSection fxtab1;
+//3 things it can do:
+    //1) Sequence
+    void sequenceNotesOn();
+    //2) Play sounds
+    void playSound(Synthesizer(std::string brand()));
+    //3) Arpeggiate
+    void synthArpeggiate(int style = 1); //1=up 2=down
+};
+
+
 
  /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
